94a648bb7916e46788c6af57ac355c262b61e395
AndreyKapus <kapustnikov@ukr.net>
2023-04-27 15:40:18 +0300

    NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+export const NavBar = () => {
+    return(
+        <div>
+            <h1>test redux app</h1>
+            <div>log in</div>
+            <button type="button">Log in</button>
+        </div>
+    )
+}
+
+export default NavBar;
