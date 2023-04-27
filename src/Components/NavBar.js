1f437009dd46f581fcb648b6acb9efa0a2db0d80
AndreyKapus <kapustnikov@ukr.net>
2023-04-27 16:16:09 +0300

    dispatch

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,18 @@
+import { useDispatch } from "react-redux";
+import { login } from "../Redux/authSlice";
+
 export const NavBar = () => {
+    const dispatch = useDispatch()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        dispatch(login(form.elements.login.value))
+    }
     return(
         <div>
             <h1>test redux app</h1>
             <div>log in</div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" name="login"/>
                 <button type="submit">Log in</button>
             </form>
